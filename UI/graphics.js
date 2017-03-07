7dbebbfbc17fcb90ae3b525ef7b689c0a3760d7e
Andrew Rumiantsev <andy1116286@gmail.com>
2017-03-07 17:55:38 +0300

    Major bug fixes
    Finally fixed some bugs with drawing and moving. Placed some inline
    styles in classes

--- UI/graphics.js
+++ UI/graphics.js
@@ -1,26 +1,24 @@
 /**
  * Created by akrum on 01.03.17.
  */
-var globalRope0Position=0;
+var globalRopePosition=0;
 var globalRope1Position=screen.width;
 var globalSignsPosition=0;
+var canUseVerticalScrolling=true;
 var mouseEvent= function (evnt) {
-        // console.log(evnt.deltaX);
-        if((globalRope0Position<=(-1*screen.width))&&evnt.deltaX>0)globalRope0Position=globalRope0Position+2*screen.width;
-        if((globalRope1Position<=(-1*screen.width))&&evnt.deltaX>0)globalRope1Position=globalRope1Position+2*screen.width;
-
-        if((globalRope0Position>=screen.width)&&evnt.deltaX<0)globalRope0Position=globalRope0Position-2*screen.width;
-        if((globalRope1Position>=screen.width)&&evnt.deltaX<0)globalRope1Position=globalRope1Position-2*screen.width;
-
-        globalRope0Position-=evnt.deltaX/2;
-        globalRope1Position-=evnt.deltaX/2;
+        if(evnt.deltaX!=0) canUseVerticalScrolling=false;
         globalSignsPosition-=evnt.deltaX/2;
-        document.getElementById("pageRope0").style.left=globalRope0Position+"px";
-        document.getElementById("pageRope1").style.left=globalRope1Position+"px";
+        globalRopePosition-=evnt.deltaX/2;
+        if(canUseVerticalScrolling)
+        {
+            globalSignsPosition-=evnt.deltaY/2;
+            globalRopePosition-=evnt.deltaY/2;
+        }
+        document.getElementById("pageRope").style.backgroundPositionX=globalRopePosition+"px";
         document.getElementById("pageSigns").style.left=globalSignsPosition+"px";
 
 };
 
-document.getElementById("ropeAndSigns").addEventListener("wheel",mouseEvent);
-console.log("position:"+parseInt(document.getElementById("pageRope0").style.left,10));
-document.getElementById("pageSigns").style.top=document.getElementById("pageRope0").style.top;
+document.body.addEventListener("wheel",mouseEvent);
+// console.log("position:"+parseInt(document.getElementById("pageRope0").style.left,10));
+// document.getElementById("pageSigns").style.top=document.getElementById("pageRope0").style.top;
