5870b7e67941e8e23529c8d137c1fca351e816f0
Andrew Rumiantsev <andy1116286@gmail.com>
2017-03-27 11:05:11 +0300

    Made task 7

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by akrum on 27.03.17.
+ */
+var express = require('express');
+var path = require('path');
+var app = express();
+var portNumber = 3000;
+app.use(express.static('public/UI'));
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/public/UI/index.html');
+});
+app.get('/styles.css',function(req,res)
+{
+    res.sendfile(__dirname+'/public/UI/styles.css');
+});
+app.listen(portNumber, function () {
+console.log('Flight is listening on port:'+portNumber)
+});
