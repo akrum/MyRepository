1e88624f8c93389c15bdc38c53442071cadb8d18
Andrew Rumiantsev <andy1116286@gmail.com>
2017-03-27 20:30:25 +0300

    changed port to heroku port

--- app.js
+++ app.js
@@ -4,15 +4,15 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var portNumber = 3000;
+var portNumber = process.env.PORT || 3000;
 app.use(express.static('public/UI'));
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/public/UI/index.html');
 });
-app.get('/styles.css',function(req,res)
-{
-    res.sendfile(__dirname+'/public/UI/styles.css');
-});
+// app.get('/styles.css',function(req,res)
+// {
+//     res.sendfile(__dirname+'/public/UI/styles.css');
+// });
 app.listen(portNumber, function () {
 console.log('Flight is listening on port:'+portNumber)
 });
