1ddf2f38876d6ba3de1fc1946610d371f122e40b
Andrew Rumiantsev <andy1116286@gmail.com>
2017-05-21 18:54:08 +0300

    Fixed vkauth redirect link
    but vkauth doesn’t work

--- app.js
+++ app.js
@@ -5,30 +5,14 @@ var dbFiller = require(__dirname + "/dbFiller.js");
 var express = require('express');
 var favicon = require('serve-favicon');
 var db = require('diskdb');
+var session = require('express-session')
 
 var artService = require(__dirname + '/private/SERVER LOGIC/articleService.js');
 var flightUserService = require(__dirname + '/private/SERVER LOGIC/userAuthService.js');
 var path = require('path');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-const VKontakteStrategy = require('passport-vkontakte').Strategy;
-passport.use(new VKontakteStrategy({
-    clientID:     5999205, // VK.com docs call it 'API ID', 'app_id', 'api_id', 'client_id' or 'apiId' 
-    clientSecret: "DAjz4ZYV4VfYyB7SEnIZ",
-    // callbackURL:  "https://flight-news.herokuapp.com/auth/vkontakte/callback",
-    callbackURL:  "http://localhost:3000/auth/vk/callback",
-    lang:"en"
-  },
-  function(accessToken, refreshToken, params, profile, done) {
-    // console.log(params.email); // getting the email 
-    
-    console.log("Got profile info:\n");
-    console.log(profile);
-    // User.findOrCreate({ vkontakteId: profile.id }, function (err, user) {
-    //   return done(err, user);
-    // });
-  }
-));
+
 
 artService.articleService.init(__dirname + '/private/articleStorage');
 flightUserService.init(__dirname + '/private/articleStorage');
@@ -36,13 +20,69 @@ flightUserService.init(__dirname + '/private/articleStorage');
 var app = express();
 app.use(favicon(__dirname + '/public/favicon.ico'));
 var portNumber = process.env.PORT || 3000;
-
-//we need it to parse content-type application/json
 app.use(bodyParser.json());
-//we need it to parse content-type application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// use passport session
+app.use(passport.initialize());
+// app.use(require('express-session')({ secret: 'accessToken' }));
+app.use(passport.session());
+
+
+//=====vk stategy
+const VKontakteStrategy = require('passport-vkontakte').Strategy;
+passport.use(new VKontakteStrategy({
+    clientID: 5999205, // VK.com docs call it 'API ID', 'app_id', 'api_id', 'client_id' or 'apiId' 
+    clientSecret: "DAjz4ZYV4VfYyB7SEnIZ",
+    callbackURL: "https://flight-news.herokuapp.com/auth/vkontakte/callback",
+    // callbackURL: "http://127.0.0.1:3000/auth/vk/callback",
+    lang: "en"
+},
+    function (accessToken, refreshToken, profile, done) {
+        console.log("accessToken: "+accessToken);
+        console.log("refresh token: "+refreshToken);
+        console.log("done: "+done);
+        // console.log(profile);
+        
+        return done(null, profile);
+    }
+));
+app.get('/auth/vkontakte', passport.authenticate('vkontakte', {
+    successRedirect: '/',
+    failureRedirect: '/errorPage',
+    failureFlash:true,
+    successFlash:true
+    //scope: ['email'] 
+}));
+
+app.get('/auth/vkontakte/callback/',
+      passport.authenticate('vkontakte', { 
+        failureRedirect: '/errorPages'
+        //scope: ['email'] 
+      }),
+        function(req, res) {
+    console.log("[OAuth2:redirect:query]:", JSON.stringify(req.query));
+      console.log("[OAuth2:redirect:body]:", JSON.stringify(req.body))
+          // Successful authentication, redirect home.
+        //   res.cookie("someString",req.query.code);
+          res.redirect('/');
+        });
+passport.serializeUser(function (user, done) {
+    console.log("serialise user is fired and user is:");
+    console.log(user);
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    console.log("deserialise user is fired");
+    done(null, obj);
+});
+//==end vk stategy
+
+
 app.get('/', function (req, res) {
     app.use(express.static('public/UI'));
+    console.log("got access token: "+ req.user);
     res.sendFile(__dirname + '/public/UI/index.html');
 });
 app.get('updatedStyles.css', function (req, res) {
@@ -62,7 +102,12 @@ app.post('/accountLogin', function (req, res) {
     }
     else {
         var checkResult = flightUserService.doesHaveAUserWithPassword({ nickname: iNickname, password: iPassword });
-        res.json({ checkSucceded: checkResult.verdict, forUser: iNickname, picture: flightUserService.getUserPictureURL(iNickname), thisSessionToken: checkResult.sessionToken });
+        res.json({
+            checkSucceded: checkResult.verdict,
+            forUser: iNickname,
+            picture: flightUserService.getUserPictureURL(iNickname),
+            thisSessionToken: checkResult.sessionToken
+        });
         res.status(200);
     }
 })
@@ -71,11 +116,17 @@ app.get('/isLoggedIn', function (req, res) {
     let querySessionToken = req.query.sessionToken || req.body.sessionToken;
     if (!iNickname || !querySessionToken) {
         res.status(400);
-        res.json({ errordescription: "check request details", logged: false });
+        res.json({
+            errordescription: "check request details",
+            logged: false
+        });
         res.end();
     }
     else {
-        res.json({ logged: flightUserService.isUserLoggedIn(iNickname, querySessionToken), forUser: iNickname });
+        res.json({
+            logged: flightUserService.isUserLoggedIn(iNickname, querySessionToken),
+            forUser: iNickname
+        });
         res.status(200);
     }
 });
@@ -83,11 +134,15 @@ app.get('/logOutUser', function (req, res) {
     let iNickname = req.query.nickname || req.body.nickname;
     if (!iNickname) {
         res.status(400);
-        res.json({ errordescription: "check request details", logged });
+        res.json({
+            errordescription: "check request details", logged
+        });
         res.end();
     }
     else {
-        res.json({ reqResult: flightUserService.logOutUserWithNickname(iNickname) });
+        res.json({
+            reqResult: flightUserService.logOutUserWithNickname(iNickname)
+        });
         res.status(200);
     }
 });
@@ -105,16 +160,24 @@ app.post('/addArticle', function (req, res) {
     let article = req.query.article || req.body.article;
     //should add a check
     if (!iNickname || !querySessionToken || !article) {
-        res.json = ({ verdict: false, explanation: "bad Request" });
+        res.json = ({
+            verdict: false,
+            explanation: "bad Request"
+        });
         res.status(400);
     }
     else {
         if (flightUserService.isUserLoggedIn(iNickname, querySessionToken)) {
-            res.json({ verdict: artService.articleService.addArticle(article) });
+            res.json({
+                verdict: artService.articleService.addArticle(article)
+            });
             res.status(200);
         }
         else {
-            res.json({ verdict: false, explanation: "User is not logged in. Log in again" });
+            res.json({
+                verdict: false,
+                explanation: "User is not logged in. Log in again"
+            });
             res.status(200);
         }
     }
@@ -136,7 +199,10 @@ app.delete('/deleteArticle', function (req, res) {
             res.json({ verdict: artService.articleService.removeArticle(idToDelete) });
         }
         else {
-            res.json({ verdict: false, explanation: "User is not logged in. Log in again" });
+            res.json({
+                verdict: false,
+                explanation: "User is not logged in. Log in again"
+            });
             res.status(200);
         }
     }
@@ -148,7 +214,7 @@ app.get("/getUserPicture", function (req, res) {
         res.status(400);
     }
     else {
-        res.json({userPicture:flightUserService.getUserPictureURL(iNickname)});
+        res.json({ userPicture: flightUserService.getUserPictureURL(iNickname) });
         res.status(200);
     }
 });
@@ -159,35 +225,14 @@ app.get("/cleanDB", function (req, res) {
     res.json({ result: "OK" });
     res.status(200);
 });
-// passport.use(new VKontakteStrategy(
-//   {
-//     clientID:     VKONTAKTE_APP_ID, // VK.com docs call it 'API ID', 'app_id', 'api_id', 'client_id' or 'apiId'
-//     clientSecret: VKONTAKTE_APP_SECRET,
-//     callbackURL:  "http://localhost:3000/auth/vkontakte/callback"
-//   },
-//   function myVerifyCallbackFn(accessToken, refreshToken, params, profile, done) {
 
-//     // Now that we have user's `profile` as seen by VK, we can
-//     // use it to find corresponding database records on our side.
-//     // Also we have user's `params` that contains email address (if set in 
-//     // scope), token lifetime, etc.
-//     // Here, we have a hypothetical `User` class which does what it says.
 
-//     // User.findOrCreate({ vkontakteId: profile.id })
-//     //     .then(function (user) { done(null, user); })
-//     //     .catch(done);
-//   }
-// ));
-app.get('/auth/vkontakte',
-  passport.authenticate('vkontakte', { display: 'popup' }),
-  function(req, res){
+app.get('/errorPage', function (req, res) {
+    res.send("<strong>Got ERROR when authorising</strong>");
 });
-app.get('/auth/vkontakte/callback',
-  passport.authenticate('vkontakte', { failureRedirect: '/login' }),
-  function(req, res) {
-    console.log(req);
-    res.redirect("/");
-  });
+
+
+
 app.listen(portNumber, function () {
     console.log('Flight is listening on port:' + portNumber);
 });
